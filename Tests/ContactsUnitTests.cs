0cdc774688f74edb81921d3c967b68344d330a30
Linda Motaung <motaugn.linda@gmail.com>
2020-06-08 04:10:15 +0200

    Final commit

--- Tests/ContactsUnitTests.cs
+++ Tests/ContactsUnitTests.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OBS_PhoneBookService.Controllers;
+using OBS_PhoneBookService.Models.OutViewModels;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -9,15 +13,21 @@ namespace Tests
     {
         //Arrange
         private int contactID = 0;
+        private ContactsController contactsController = new ContactsController();
+        private OBSEntities db = new OBSEntities();
 
         [TestMethod]
         public void GetListOfContacts()
         {
-            //Arrange
-
             //Act
+            var result = contactsController.Get().ToList();
 
             //Assert
+            List<Contact> dbResult = new List<Contact>();
+            using (db)
+                dbResult = db.Contacts.ToList();
+
+            Assert.IsTrue(result.Equals(dbResult));
         }
 
         [TestMethod]
@@ -26,20 +36,41 @@ namespace Tests
             //Arrange
             contactID = 1;
 
-            //Act
-            ContactsController contactsController = new ContactsController();
+            //Act         
             var result = contactsController.Get(contactID);
+
             //Assert
+            Contact getSingleRecord = new Contact();
+            using (db)
+            {
+                getSingleRecord = db.Contacts.FirstOrDefault(x => x.ID == contactID);              
+            }
+
+            Assert.IsTrue(getSingleRecord != null);
         }
 
         [TestMethod]
         public void CreateContact()
         {
             //Arrange
+            ContactsViewModel contactsModel = new ContactsViewModel();
+            contactsModel.FirstName = "Linda";
+            contactsModel.Surname = "Motaung";
+            contactsModel.Tel = "0827360066";
+            contactsModel.Cell = "0827360066";
+            contactsModel.Email = "motaung.linda@gmail.com";
+
+            string expected = contactsModel.FirstName;
 
             //Act
+            var result = contactsController.Create(contactsModel);
 
             //Assert
+            Contact dbResult = new Contact();
+            using (db)
+                dbResult = db.Contacts.FirstOrDefault(x => x.FirstName.Contains(expected));
+
+            Assert.IsTrue(dbResult != null);
         }
 
         [TestMethod]
@@ -47,10 +78,25 @@ namespace Tests
         {
             //Arrange
             contactID = 1;
+            ContactsViewModel contactsModel = new ContactsViewModel();
+            contactsModel.FirstName = "Linda";
+            contactsModel.Surname = "Frank"; //Updated surname
+            contactsModel.Tel = "0827360066";
+            contactsModel.Cell = "0827360066";
+            contactsModel.Email = "motaung.linda@gmail.com";
+
+            string expected = contactsModel.FirstName;
+            string updatedSurname = contactsModel.Surname;
 
             //Act
+            var result = contactsController.Update(contactsModel);
 
             //Assert
+            Contact getUpdatedValue = new Contact(); ;
+            using (db)
+                getUpdatedValue = db.Contacts.FirstOrDefault(x => x.FirstName == expected);
+
+            Assert.IsTrue(getUpdatedValue.Surname == updatedSurname);
         }
 
         [TestMethod]
@@ -58,26 +104,35 @@ namespace Tests
         {
             //Arrange
             contactID = 1;
+            ContactsViewModel contactsModel = new ContactsViewModel();
+            contactsModel.Id = contactID;
+            int expected = contactsModel.Id;
 
             //Act
+            var deleteResult = contactsController.Delete(contactsModel);
 
             //Assert
+            Contact deletedRecord = new Contact();
+            using (db)
+                deletedRecord = db.Contacts.FirstOrDefault(x => x.ID == expected);
+
+            Assert.IsTrue(deletedRecord == null);
         }
 
         /*
        When we try tp get an invidivual contact in the address book by passing in an invalid contact ID, this will cause a NullReference Exception
        This is an expected exception that we are testing against
        */
-        //[TestMethod]
-        //[ExpectedException(typeof(NullReferenceException))]
-        //public void InvalidContactRequest()
-        //{
-        //    //Arrange
-        //    contactID = 0;
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void InvalidContactRequest()
+        {
+            //Arrange
+            contactID = 0;
 
-        //    //Act
-        //    ContactsController contactsControllr = new ContactsController();
-        //    contactsControllr.Get(contactID);
-        //}
+            //Act
+            ContactsController contactsControllr = new ContactsController();
+            contactsControllr.Get(contactID);
+        }
     }
 }
